2e7c852c311abc033006fd4bb69adfa584ad5431
0x4bd0 <abdoumjr@gmail.com>
2021-02-27 22:56:00 +0100

    added home screen

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,4 +1,6 @@
 import NavBar from "../components/navBar";
+import HomePageImage from "../images/homeImage";
+import '../styles/home.css'
 
 export interface HomeProps {
     
@@ -7,7 +9,15 @@ export interface HomeProps {
 const Home: React.FC<HomeProps> = () => {
     return ( 
         <div className="home">
-             <h1>hi</h1>
+             <div  className="leftDiv">
+                  <div className="homePageImage">
+                  <HomePageImage></HomePageImage>
+                  </div>
+             </div>
+             <div className="rightDiv">
+                  <h1>Foody</h1>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+             </div>
         </div>
       
      );
